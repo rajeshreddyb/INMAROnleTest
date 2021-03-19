9c407368744d6a9a42da839044d95ec1b980ab71
rajeshreddyb <rajeshreddy.bijjam@gmail.com>
2021-03-19 20:10:51 +0530

    Add files via upload

---
+++ Offer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INMAR_OnlineTest_WebApi.Models
+{
+    public class Offer
+    {
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INMAR_OnlineTest_WebApi.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public static List<Product> GetProducts()
+        {
+
+            return new List<Product>()
+            {
+                new Product { ProductName = "P1", Price = 1000, Description = "P1 Desc" }
+        }
+    }
+}
